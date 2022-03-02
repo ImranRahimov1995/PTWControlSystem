85e53e4820d745cd5c69c6604fdb23d15e93ea6f
ImranRahimov1995 <imash.odessahouse@gmail.com>
2022-03-02 16:01:07 +0400

    BUG: profile create if user is created , and user is not admin

--- project/apps/users/signals.py
+++ project/apps/users/signals.py
@@ -4,7 +4,17 @@ from django.dispatch import receiver
 from apps.users.models import User, Profile
 
 
-@receiver(post_save,sender=User)
+@receiver(post_save, sender=User)
 def profile_create(sender, **kwargs):
-    print(kwargs)
-    Profile.objects.create(user=kwargs['instance'])
+    created = kwargs['created']
+    user = kwargs.get('instance', None)
+    print(created)
+    if user:
+
+        if user.is_staff:
+            return False
+        if not created:
+            return False
+        else:
+            print('popal')
+            Profile.objects.create(user=user)
