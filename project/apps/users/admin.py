58edfe5a214ba7739bc9090eeb2c0f606522150d
ImranRahimov1995 <imash.odessahouse@gmail.com>
2022-03-02 16:01:50 +0400

    ADMIN: Customize UserAdmin

--- project/apps/users/admin.py
+++ project/apps/users/admin.py
@@ -1,3 +1,31 @@
 from django.contrib import admin
+from .models import User, Profile
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+
+class CustomAdmin(UserAdmin):
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": ("username", "password1", "password2", "email",
+                           'first_name', 'last_name'),
+            },
+        ),
+    )
+
+
+admin.site.register(User, CustomAdmin)
+
+
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = (
+    'id', 'user', 'code', 'duty', 'company', 'phone', 'created')
+    list_filter = ('duty', 'sex', 'company',)
+
+    search_fields = ('email',)
+    ordering = ('-created',)
+
+
+admin.site.register(Profile, ProfileAdmin)
